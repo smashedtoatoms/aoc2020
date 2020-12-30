4c883b933290bf3a2916f935b04c489442f1fa65
Jason Legler <j@legler.io>
2020-12-30 15:39:15 -0700

    day-3

--- lib/input-file/src/lib.rs
+++ lib/input-file/src/lib.rs
@@ -10,15 +10,19 @@ pub struct Input {
 }
 
 impl Input {
+    /// Creates a new Input containing the contents of the file at the
+    /// specified path.
     pub fn new(path: &Path) -> Result<Self> {
         let contents = fs::read_to_string(path)?;
         Ok(Self { contents })
     }
 
+    /// Returns an iterator of strings split by the specified delimiter.
     pub fn strings<'a>(&'a self, delimiter: &'a str) -> impl Iterator<Item = &str> + 'a {
         self.contents.split(delimiter).filter(|x| !x.is_empty())
     }
 
+    /// Returns an iterator of numbers split by the specified delimiter.
     pub fn numbers<'a, N: std::str::FromStr>(
         &'a self,
         delimiter: &'a str,
@@ -26,6 +30,8 @@ impl Input {
         self.strings(delimiter).filter_map(|x| x.parse().ok())
     }
 
+    /// Creates a new input containing the contents of the file specified in
+    /// the first argument passed to the program.
     pub fn from_args() -> Result<Self> {
         let mut args = env::args().skip(1);
         let arg = args.next().unwrap_or_else(|| DEFAULT_FILE.to_string());
--- src/day-3/src/main.rs
+++ src/day-3/src/main.rs
@@ -3,42 +3,73 @@ use input_file::Input;
 
 /// AOC3
 fn main() -> Result<()> {
+    let input = Input::from_args()?;
+    let tile: Vec<&str> = input.strings("\n").collect();
+    println!(
+        "Tree Count:         {:?}\nTree Count Product: {:?}\n",
+        find_trees(&tile, 3, 1)?,
+        find_multi_slope_product(&tile)?
+    );
     Ok(())
 }
 
 /// Returns number of trees encountered in map.
-fn find_trees(tree_map: &[&str]) -> Result<usize> {
-    let mut x: usize = 3;
+fn find_trees(tile: &[&str], x_size: usize, y_size: usize) -> Result<usize> {
+    let mut x: usize = 0;
+    let mut y: usize = 0;
     let mut tree_count: usize = 0;
+    let tile_width = (tile
+        .get(0)
+        .ok_or_else(|| anyhow!("failed to pull first column"))?)
+    .len();
 
-    for raw_row in tree_map {
-        let row: Vec<char> = raw_row.chars().collect();
+    while y < tile.len() {
+        let row: Vec<char> = tile
+            .get(y)
+            .ok_or_else(|| anyhow!("failed to pull row"))?
+            .chars()
+            .collect();
         let spot = row.get(x).ok_or_else(|| anyhow!("failed to pull column"))?;
         if spot == &'#' {
             tree_count += 1;
         }
-        x += 3
+        if x + x_size >= tile_width {
+            x = (x + x_size) - tile_width;
+        } else {
+            x += x_size;
+        }
+        y += y_size;
     }
 
     Ok(tree_count)
 }
 
+/// Returns the product of the trees encountered using the various specified
+/// routes.
+fn find_multi_slope_product(tree_map: &[&str]) -> Result<usize> {
+    Ok(find_trees(tree_map, 1, 1)?
+        * find_trees(tree_map, 3, 1)?
+        * find_trees(tree_map, 5, 1)?
+        * find_trees(tree_map, 7, 1)?
+        * find_trees(tree_map, 1, 2)?)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::find_trees;
+    use crate::{find_multi_slope_product, find_trees};
 
     #[test]
     fn test_condition_1() {
-        assert_eq!(find_trees(&get_test_data()), 7);
+        assert_eq!(find_trees(&get_test_data(), 3, 1).unwrap(), 7);
     }
 
     #[test]
     fn test_condition_2() {
-        assert_eq!(1, 1);
+        assert_eq!(find_multi_slope_product(&get_test_data()).unwrap(), 336);
     }
 
     /// Returns test data.
-    fn get_test_data() -> Vec<&str> {
+    fn get_test_data<'a>() -> Vec<&'a str> {
         vec![
             "..##.......",
             "#...#...#..",
